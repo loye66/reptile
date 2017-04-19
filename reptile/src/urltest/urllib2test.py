48d9dec04b520349a10899da79eadff57b7071c2
loye66 <791324651@qq.com>
2017-04-19 17:09:02 +0800

    urllib2test

---
+++ reptile/src/urltest/urllib2test.py
@@ -0,0 +1,35 @@
+#coding:utf-8
+'''
+Created on 2017年4月19日
+
+@author: liuyaqing
+'''
+
+import urllib2
+from lib2to3.pgen2.tokenize import cookie_re
+import cookielib
+from setuptools.ssl_support import opener_for
+from pip._vendor.requests.packages.urllib3 import response
+
+url="http://www.baidu.com"
+
+print '第一种方法'
+response1=urllib2.urlopen(url)
+print response1.getcode()#打印状态码
+print len(response1.read())
+
+print"第二种方法"
+request = urllib2.Request(url) 
+request.add_header("user-agent","Mozilla/5.0")
+response2=urllib2.urlopen(request)
+print response2.getcode()
+print len(response2.read())
+
+print "第三种方法"
+cj=cookielib.CookieJar()
+opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+urllib2.install_opener(opener)
+response3=urllib2.urlopen(url)
+print response3.getcode()
+print cj
+print response3.read()
