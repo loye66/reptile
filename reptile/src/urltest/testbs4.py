2c01ab851458ab2a2286ad16da76af11be2fc666
loye66 <791324651@qq.com>
2017-04-20 10:25:23 +0800

    testbs4

---
+++ reptile/src/urltest/testbs4.py
@@ -0,0 +1,42 @@
+#coding:utf-8
+'''
+Created on 2017年4月19日
+
+@author: liuyaqing
+'''
+from bs4 import BeautifulSoup
+from pip.index import Link
+from setuptools.package_index import HREF
+import re
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+soup=BeautifulSoup(html_doc,'html.parser',from_encoding='utf-8')
+
+print '获取所有的链接'
+links=soup.find_all('a')
+for link in  links :
+    print link.name,link['href'],link.get_text()
+    
+print'获取lacie的链接'
+link_node=soup.find('a',href='http://example.com/lacie')
+print link_node.name,link_node['href'],link_node.get_text()
+
+print '正则匹配'
+link_node=soup.find('a', href=re.compile(r"ill"))
+print link_node.name,link_node['href'],link_node.get_text()
+
+print '获取P段落文章'
+p_123=soup.find('p',class_="title")
+print p_123.name,p_123.get_text()
--- reptile/src/urltest/urllib2test.py
+++ reptile/src/urltest/urllib2test.py
@@ -14,9 +14,9 @@ from pip._vendor.requests.packages.urllib3 import response
 url="http://www.baidu.com"
 
 print '第一种方法'
-response1=urllib2.urlopen(url)
+response1=urllib2.urlopen(url)#网页下载
 print response1.getcode()#打印状态码
-print len(response1.read())
+print len(response1.read())#打印返回网页内容的长度，read读取长度
 
 print"第二种方法"
 request = urllib2.Request(url) 
@@ -26,10 +26,10 @@ print response2.getcode()
 print len(response2.read())
 
 print "第三种方法"
-cj=cookielib.CookieJar()
-opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
+cj=cookielib.CookieJar()#存储cookie数据
+opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))#创建容器
+urllib2.install_opener(opener)#生成处理器
 response3=urllib2.urlopen(url)
 print response3.getcode()
 print cj
-print response3.read()
+print response3.read()#输出cookie
